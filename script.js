c472a228b2bdf9dac891d688376887cbbb420837
Rahil Abdul <153207425+Rahil399@users.noreply.github.com>
2024-12-26 13:12:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
-}
+	{ 
+		arr.sort((a,b) => a - b);
+
+	    let totalCost = 0;
+	
+	    while(arr,length > 1){
+	        const smallest1 = arr.shift();
+	        const smallest2 = arr.shift();
+	
+	        const cost = smallest1 + smallest2;
+	        totalCost += cost;
+	
+	        arr.push(cost);
+	
+	
+	        arr.sort((a,b) => a -b);
+	    } 
+	}
 
 module.exports=mincost;
